765e0e1b088f4d42d1dc935d08c9dac718409058
Joy Biswas <joybiswas389@gmail.com>
2025-01-04 23:28:16 +0600

    updated sidebar ui

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -2,23 +2,29 @@ import React from 'react'
 import { ScrollArea } from '../ui/scroll-area'
 
 
-const tags = Array.from({ length: 60 }).map(
+const tags = Array.from({ length: 5 }).map(
     (_, i, a) => `v1.2.0-beta.${a.length - i}`
 )
 
 
 const Sidebar = () => {
+    const items = <>{tags.map((tag) => (
+        <>
+            <div key={tag} className="text-sm hover:bg-gray-900 hover:bg-opacity-15 px-3 py-2 rounded-sm my-2 cursor-pointer hover:text-red-500">
+                {tag}
+            </div>
+        </>
+    ))}</>
     return (
         <ScrollArea className="h-screen w-64  text-white border-gray-700 border-x-[0.2px]">
-            <div className="p-4">
-                <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
-                {tags.map((tag) => (
-                    <>
-                        <div key={tag} className="text-sm">
-                            {tag}
-                        </div>
-                    </>
-                ))}
+            <div className="py-4 mx-4" style={{ fontFamily: 'var(--font-manrope)' }}>
+                <h4 className="mb-4 font-medium leading-none uppercase text-gray-300">Problems</h4>
+                {items}
+            </div>
+
+            <div className="py-4 mx-4" style={{ fontFamily: 'var(--font-manrope)' }}>
+                <h4 className="mb-4 font-medium leading-none uppercase text-gray-300">Favourites</h4>
+                {items}
             </div>
         </ScrollArea>
     )
